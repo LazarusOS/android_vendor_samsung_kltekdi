bb4aaa1bbc7f0964c57274ee156ad8181315d447
GaHoKwan <kwangaho@gmail.com>
2015-12-12 18:30:22 +0800

    kltekdi: add missing gps dependent libs

--- kltekdi-vendor-blobs.mk
+++ kltekdi-vendor-blobs.mk
@@ -23,20 +23,24 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/kltekdi/proprietary/etc/sap.conf:system/etc/sap.conf \
     vendor/samsung/kltekdi/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/samsung/kltekdi/proprietary/lib/libgps.utils.so:system/lib/libgps.utils.so \
-    vendor/samsung/kltekdi/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
-    vendor/samsung/kltekdi/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
     vendor/samsung/kltekdi/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
-    vendor/samsung/kltekdi/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
-    vendor/samsung/kltekdi/proprietary/lib/libperipheral_client.so:system/lib/libperipheral_client.so \
+    vendor/samsung/kltekdi/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
+    vendor/samsung/kltekdi/proprietary/lib/libloc_ds_api.so:system/lib/libloc_ds_api.so \
+    vendor/samsung/kltekdi/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libizat_core.so:system/vendor/lib/libizat_core.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so \
+    vendor/samsung/kltekdi/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
+    vendor/samsung/kltekdi/proprietary/lib/libperipheral_client.so:system/lib/libperipheral_client.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
+    vendor/samsung/kltekdi/proprietary/vendor/lib/libdsi_netctrl.so:system/vendor/lib/libdsi_netctrl.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libidl.so:system/vendor/lib/libidl.so \
+    vendor/samsung/kltekdi/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
+    vendor/samsung/kltekdi/proprietary/vendor/lib/libqdi.so:system/vendor/lib/libqdi.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libqmi.so:system/vendor/lib/libqmi.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libqmi_cci.so:system/vendor/lib/libqmi_cci.so \
     vendor/samsung/kltekdi/proprietary/vendor/lib/libqmi_client_qmux.so:system/vendor/lib/libqmi_client_qmux.so \
